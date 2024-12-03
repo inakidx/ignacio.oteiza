d86f75e33a44c1f7ffe61a5230ceff928fe0851b
inakidx <inakidx@gmail.com>
2024-12-03 18:58:04 +0100

    Fix assetPrefix
    Debe empezar por / o por http/https

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: './', // Necesario para GitHub Pages
+  assetPrefix: '/', // Necesario para GitHub Pages
   images: {
     unoptimized: true, // Requerido si usas imágenes en Next.js
   },
