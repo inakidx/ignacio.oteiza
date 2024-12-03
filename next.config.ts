9bde6d76b0e281eeb7a7eaf35bc22f2a5397d89f
inakidx <inakidx@gmail.com>
2024-12-03 19:10:37 +0100

    Fix add basePath
    Add basePath to publish in github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/',
   assetPrefix: '/', // Necesario para GitHub Pages
   images: {
     unoptimized: true, // Requerido si usas imágenes en Next.js
