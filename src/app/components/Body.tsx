5bce5e321ec06dbe64f0bf5df32910a6420c8a97
inakidx <inakidx@gmail.com>
2024-12-06 00:23:15 +0100

    Update under development

--- src/app/components/Body.tsx
+++ src/app/components/Body.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 const Body = () => {
     return (
-        <div className='grid justify-center items-center content-center w-dvw h-dvh'>
-            <div className='flex flex-row items-center'>
-                <img src='yopeFine.jpeg' className='border-4 border-gray-400 rounded w-2/5 float-left' />
-                <label className="text-5xl">Under development
-                    <div className='animate-bounce inline-block'>.</div>
-                    <div className='animate-bounce inline-block delay-500'>.</div>
-                    <div className='animate-bounce inline-block delay-700'>.</div>
-                </label>
-            </div>
+        <div className='flex w-dvw h-dvh flex-wrap justify-center items-center content-center'>
+            <img src='yopeFine.jpeg' className='border-4 border-gray-400 rounded-full h-3/5 float-left aspect-square' />
+            <label className="text-5xl">Under development
+                <div className='animate-bounce inline-block'>.</div>
+                <div className='animate-bounce inline-block animation-delay-200'>.</div>
+                <div className='animate-bounce inline-block animation-delay-400'>.</div>
+            </label>
         </div>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,12 @@
 import type { Config } from "tailwindcss";
+import plugin from 'tailwindcss/plugin';
+import typography from '@tailwindcss/typography'
+const delayValues = {
+  '100': '100ms',
+  '200': '200ms',
+  '300': '300ms',
+  // Agrega más valores según necesites
+};
 
 export default {
   content: [
@@ -13,7 +21,25 @@ export default {
         foreground: "var(--foreground)",
         primary: "#95b8ff"
       },
+      animationDelay: {
+        '0': '0s',
+        '100': '0.1s',
+        '200': '0.2s',
+        '300': '0.3s',
+        '400': '0.4s',
+        '500': '0.5s',
+        '1000': '1s',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    typography,
+    plugin(function ({ addUtilities }) {
+      Object.values(delayValues).map(value => ({
+        [`animation-delay-${value}`]: {
+          animationDelay: value,
+        },
+      }))
+    }),
+  ],
 } satisfies Config;
