89c3de7cac15002273c0a3e8c67f44c7047d029d
inakidx <inakidx@gmail.com>
2024-12-14 11:00:40 +0100

    Update remove warning lints

--- src/app/components/Perfil.tsx
+++ src/app/components/Perfil.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Perfil = () => {
     return (
@@ -6,7 +7,7 @@ const Perfil = () => {
         flex flex-col lg:items-end items-center'>
             <div className='flex flex-col items-center bg-gray-300 rounded-3xl'>
                 {/* <div className='rounded-full aspect-square bg-blue-400 w-48 h-48 lg:w-96 lg:h-96 mt-5'></div> */}
-                <img className='w-48 lg:w-96 mt-5' src='perfil.png'></img>
+                <Image className='w-48 lg:w-96 mt-5' src='perfil.png' alt='Picture of the author'/>
                 <div className='mt-6  lg:mx-16 text-lg lg:text-4xl font-medium'>
                     Ignacio Oteiza Solchaga
                 </div>
--- src/app/components/UnderDevelopment.tsx
+++ src/app/components/UnderDevelopment.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 const UnderDevelopment = () => {
     return (
         <div className='flex w-dvw h-dvh flex-wrap justify-center items-center content-center'>
-            <img src='yopeFine.jpeg' className='border-4 border-gray-400 rounded-full h-3/5 float-left aspect-square' />
+            <Image src='yopeFine.jpeg' alt='Picture cartoon of the author' 
+            className='border-4 border-gray-400 rounded-full h-3/5 float-left aspect-square' />
             <label className="text-5xl text-center">Under development
                 <div className='animate-bounce inline-block'>.</div>
                 <div className='animate-bounce inline-block animation-delay-200'>.</div>
